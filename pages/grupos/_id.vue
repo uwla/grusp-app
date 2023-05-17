11b74166d852dfbb7a1415d898143e81a1cfdbb3
uwla <uwlalinux@gmail.com>
2023-05-17 17:09:23 -0300

    add simple page to view single grupo

---
+++ pages/grupos/_id.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <h1>{{ grupo.titulo }}</h1>
+        <p>{{ grupo.descricao }}</p>
+        <ul>
+            <li v-for="tag,i in grupo.tags" :key="i">
+                {{ tag }}
+            </li>
+        </ul>
+    </main>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios, params }) {
+        const id = params.id
+        const url = `http://localhost:8000/api/public/grupos/${id}`
+        const grupo = (await $axios.get(url)).data
+        const data = {
+            grupo
+        }
+        return data
+    }
+}
+</script>
