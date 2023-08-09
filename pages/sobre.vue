797bfadf88408f61991a8ae27a006ae201bb3cd4
uwla <uwlalinux@gmail.com>
2023-08-09 14:00:19 -0300

    add "about" page

---
+++ pages/sobre.vue
@@ -0,0 +1,25 @@
+<template>
+    <main class="text-justify">
+        <h1>SOBRE O GRUSP</h1>
+        <p>
+            O GRUSP, abreviatura de Grupos USP, é um website destinado a reunir
+            informações sobre grupos estudantis da USP de diversas vertentes:
+            esportes, estudos, dança, cultura, religião, jogos, e vários outros.
+            É uma espécie de catalogo, no qual é possível pesquisar grupos
+            universitários com base em categorias e possivelmente ver
+            informações adicionais, tais como horário e local de encontro,
+            informações de contato, fotos, links de redes sociais, entre outras.
+        </p>
+        <p>
+            O objetivo inicial é que estudantes possam achar grupos que
+            compartilham de seus interesses e hobbies. Para tanto, qualquer
+            estudante ou funcionário da USP pode criar uma conta no site e
+            registrar grupos do qual participa. Assim, fica mais fácil para os
+            estudantes acharem atividades que gostam ou explorar novas.
+        </p>
+        <p>
+            A ideia do website e seu desenvolvimento foram feitos por
+            <b-link href="https://github.com/uwla">uwla</b-link>.
+        </p>
+    </main>
+</template>
