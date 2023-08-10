f026dfb8837eaecb56641b2ec072eec003a19122
uwla <uwlalinux@gmail.com>
2023-08-10 09:25:43 -0300

    show helpful messages in account verification page

--- pages/conta/verificar.vue
+++ pages/conta/verificar.vue
@@ -1,8 +1,18 @@
 <template>
-    <main class="w400">
-        <big>
-            Your email is verified? {{ verified }}
-        </big>
+    <main class="w500">
+        <div v-if="verified">
+           <h1>VERIFICAÇÃO CONCLUÍDA</h1>
+           <p>Sua conta foi verifica com sucesso.</p>
+           <p>Você pode fechar esta página agora.</p>
+        </div>
+        <div v-else>
+            <h1>VERIFICAÇÃO FALHOU</h1>
+            <p>Não foi possível verificar sua conta.</p>
+            <p>
+                É possível que o link de verificação tenha expirado,
+                portanto solicite um novo link.
+            </p>
+        </div>
     </main>
 </template>
 <script>
