88c41934e725e2fd2f659f34171b551f5afb156e
uwla <uwlalinux@gmail.com>
2023-08-10 16:01:28 -0300

    add dynamic head title to match h1

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,3 +5,22 @@
         <site-footer />
     </div>
 </template>
+<script>
+export default {
+    mounted() {
+        // Get the h1 tag
+        var h1Element = document.querySelector('h1');
+
+        // not found
+        if (! h1Element)
+            return
+
+        // the text of the h1
+        var h1Text = h1Element.textContent.toLowerCase();
+
+        // Get the contents of the h1 tag
+        if (h1Text !== 'grusp')
+            document.title = `GRUSP - ${h1Text}`
+    },
+}
+</script>
